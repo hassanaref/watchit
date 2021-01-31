c90c45eeb3a53eae0b6b6c014d8cf0014ff09193
hassanaref <72562088+hassanaref@users.noreply.github.com>
2021-01-31 18:34:15 +0200

    1

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const debounce = require('lodash.debounce');
-const chokidar =  require('chokidar');
+const chokidar = require('chokidar');
 const program = require('caporal');
-const start =   debounce(() => {
-    console.log('starting program')
- }, 100);
-chokidar.watch('.')
-.on('add', start)
-.on('change', () => console.log('file changed'))
-.on('unlink', () => console.log('file unlinked'));
+const fs = require('fs');
+program.version('1').argument('[filename]', 'name of file').action(async({ filename }) => {
+    const name = filename || 'index.js';
+    try{
+    await fs.promises.access(name);}
+    catch (err){
+        throw new error('file name not incorrect');
+     }
+	const start = debounce(() => {
+		console.log('starting program');
+	}, 100);
+	chokidar
+		.watch('.')
+		.on('add', start)
+		.on('change', () => start)
+		.on('unlink', () => start);
+});
+program.parse(process.argv);
